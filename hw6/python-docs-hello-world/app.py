21127bbba5e5d17e92e79213c321b5aeab54c6ee
Chenshu Xu <newton9001@gmail.com>
2021-10-03 16:50:24 -0400

    add hw6 server apis

---
+++ hw6/python-docs-hello-world/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+api_key = "yODx9w6WUkq27c1Vb3HlQsMiUlxcTVlC"
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/timelines", methods=['GET'])
+def timelines():
+    data = request.args
+
+    return jsonify(data)
+
+
+def request_1d_data(location: str):
+    url = "https://api.tomorrow.io/v4/timelines"
+
+    querystring = {"location": location,
+                   "fields": ["temperature", "temperatureApparent", "temperatureMin", "temperatureMax", "windSpeed",
+                              "windDirection", "humidity", "pressureSeaLevel", "uvIndex", "weatherCode",
+                              "precipitationProbability", "precipitationType", "sunriseTime", "sunsetTime",
+                              "visibility", "moonPhase", "cloudCover"], "units": "imperial", "timesteps": "1d",
+                   "timezone": "America/Los_Angeles", "apikey": api_key}
+
+    headers = {"Accept": "application/json"}
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return jsonify(response.text)
+
+
+@app.route("/example")
+def example():
+    with open('example_1.json') as json_file:
+        data = json.load(json_file)
+        return jsonify(data)
