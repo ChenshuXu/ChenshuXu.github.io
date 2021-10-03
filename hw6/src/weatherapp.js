56231b612c58b7c4508f62e49187d50ffeb3095f
Chenshu Xu <newton9001@gmail.com>
2021-10-03 16:41:24 -0400

    hw6 form part done

---
+++ hw6/src/weatherapp.js
@@ -0,0 +1,82 @@
+'use strict';
+function WeatherApp() {
+    console.log('app started');
+    document.getElementById("input-form").addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+    document.getElementById("auto-detect-location").onchange = onAutoDetectLocationChange;
+
+    document.getElementById("submit").addEventListener("click", onSubmitClick);
+    document.getElementById("clear").addEventListener("click", onClearClick);
+}
+
+function onAutoDetectLocationChange () {
+    if (document.getElementById("auto-detect-location").checked) {
+        console.log("checked");
+        document.getElementById("street").disabled = true;
+        document.getElementById("city").disabled = true;
+        document.getElementById("state").disabled = true;
+    } else {
+        console.log("not checked");
+        document.getElementById("street").disabled = false;
+        document.getElementById("city").disabled = false;
+        document.getElementById("state").disabled = false;
+    }
+}
+
+function onClearClick(event) {
+    event.preventDefault();
+    console.log("clear clicked");
+    document.getElementById("street").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("state").value = "";
+}
+
+function onSubmitClick(event) {
+    let street = document.getElementById("street").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    if (document.getElementById("auto-detect-location").checked) {
+        console.log("auto detect");
+        $.ajax({
+            type: "GET",
+            url: "https://ipinfo.io/?token=5f8b30af9607e3",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                let loc = data.loc;
+                let lat = loc.split(",")[0];
+                let lng = loc.split(",")[1];
+                RequestWeatherData(lat, lng);
+            }
+        });
+    } else {
+        if (street !== "" && city !== "" && state !== "") {
+            console.log("search location");
+            console.log(street);
+            console.log(city);
+            console.log(state);
+            let address = street + '+' + city + '+' + state;
+            let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                address +
+                "&key=AIzaSyBmRnM9YBpvHa1SkTZYSZ55p5mB5v1_rFc";
+            $.ajax({
+                type: "GET",
+                url: url,
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    let lat = data.results[0].geometry.location.lat;
+                    let lng = data.results[0].geometry.location.lng;
+                    RequestWeatherData(lat, lng);
+                }
+            });
+        }
+    }
+}
+
+function RequestWeatherData(lat, lng) {
+    console.log("request weather data");
+    console.log(lat);
+    console.log(lng);
+}
